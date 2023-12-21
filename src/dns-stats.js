aa4c7c82e7f2830896c9cadd5fbbf979b94584d0
Sergii Kolesnyk <marakorissk@gmail.com>
2023-12-21 21:21:10 +0200

    docs: dream-team ready

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,10 +22,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+const domains = [
+    'code.yandex.ru',
+    'music.yandex.ru',
+    'yandex.ru'
+   ]
+
+function getDNSStats(domains) {
+  let splitDom = domains.map( el => {
+    return el.split('.')
+  })
+  let addDot = splitDom.map( (el) => {
+    return el.map( (nEl) => {
+      return `.${nEl}`;
+    }).reverse()
+  })
+  let data = []
+  addDot.forEach( (el, index) => {
+    el.forEach ((el, index) => {
+      if(index > 0) {
+        data.push(data[data.length-1]+el)
+      } else {  
+        data.push(el)
+      }
+    })
+  })
+  let dataTemp = [];
+  let ourObject = {};
+  data.forEach(el => {
+    let counter = 0;
+    let elFlag = false;
+    dataTemp.forEach(elCh => {
+      if(elCh == el) {elFlag = true};
+    })
+    if(!elFlag) {
+      dataTemp.push(el)
+      data.forEach(elc => {
+        if (elc == el) counter += 1;
+      })
+      dataTemp.push(counter)
+      ourObject[el] = counter;
+    }
+  })
+  return ourObject
 }
+
+// console.log(getDNSStats(domains));
 
 module.exports = {
   getDNSStats
--- src/dream-team.js
+++ src/dream-team.js
@@ -10,13 +10,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @example
  * 
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
- *
- */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+*
+*/
+
+// let Team = ['Olivia', 1111, 'Lily', 'Oscar', true, null, '       Gilles Deleuze','grace',]
+
+function createDreamTeam(members) {
+  if (!members || members[0] == null) {
+    return false
+  } else {
+    let newArr = [];
+    members.forEach( el => {
+      if (typeof el == "string") {
+        newArr.push(el)
+      }
+    })
+    newArr = newArr.map(el => {
+      let temp;
+      temp = el.trim().toUpperCase()
+      return temp[0]
+    })
+    return newArr.sort().join('');
+  }
 }
+
+// console.log(createDreamTeam(Team));
 
 module.exports = {
   createDreamTeam
