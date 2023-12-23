a830443ad22a52370f39867b4a337a96acafbfb7
Sergii Kolesnyk <marakorissk@gmail.com>
2023-12-23 20:05:11 +0200

    docs: extended repeater ready

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+// let str = '';
+
+
+function encodeLine(str) {
+  let split = str.split("");
+  let counter = 0;
+  let char = split[0];
+  let encodedStr = '';
+  split.forEach(el => {
+    if (el != char) {
+      if(counter > 1) {
+        encodedStr += String(counter) + char;
+      } else {
+        encodedStr += char;
+      }
+      char = el;
+      counter = 1;
+    } else if (el == char) {
+      counter += 1
+    }
+  })
+  if(counter > 1) {
+    encodedStr += String(counter) + char;
+  } else {
+    encodedStr += char;
+  }
+  if (encodedStr == 'undefined') encodedStr = '';
+  return encodedStr;
 }
+
+// console.log(encodeLine(str));
 
 module.exports = {
   encodeLine
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+// let str = 'STRING'
+// let repOpt = { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00'}
+
+function repeater(str, options) {
+  let mainStr = String(str);
+  if (String(options.addition) == "false") { options.addition = 'false'}
+  if (String(options.addition) == "null") { options.addition = 'null'}
+  let mainAddit = options.addition ? String(options.addition) : "";
+
+  if (options.separator == '' || typeof options.separator == "undefined") { options.separator = '+'}
+  if (options.additionSeparator == '' || typeof options.additionSeparator == "undefined") { options.additionSeparator = '|'}
+
+  if (options.additionRepeatTimes > 0) {
+    let i = options.additionRepeatTimes - 1;
+    while (i > 0) {
+      mainAddit += options.additionSeparator + options.addition
+      i -= 1;
+    }
+    mainStr += mainAddit;
+  } else if (mainAddit == "") {
+    mainStr += mainAddit;
+  } else if (mainAddit != "") {
+    mainStr += mainAddit;
+
+  }
+
+  let endStr = mainStr;
+  
+  if (options.repeatTimes > 0) {
+    let i = options.repeatTimes - 1;
+    while (i > 0) {
+      endStr += options.separator + mainStr
+      i -= 1;
+    }
+  }
+
+  return endStr
 }
+
+// console.log(repeater(str, repOpt));
 
 module.exports = {
   repeater
